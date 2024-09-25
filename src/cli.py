ff31a9eebc66df624458cda7d89224a2e0144d49
Foo JH <foojihaw@hotmail.com>
2024-09-26 00:53:40 +0700

    NEW: arg integration

---
+++ src/cli.py
@@ -0,0 +1,3 @@
+import consulsync
+
+consulsync.doCli()
--- src/consulsync/__init__.py
+++ src/consulsync/__init__.py
@@ -4,4 +4,25 @@ import argparse
 
 
 def doCli():
-    print ('ok')
+  parser = argparse.ArgumentParser()
+  subparser = parser.add_subparsers(dest='cmd')
+
+  UpParser = subparser.add_parser(
+    'up',
+    help = 'Upload to Consul'
+  )
+  UpParser.add_argument('-d','--dir')
+
+  DownParser = subparser.add_parser(
+    'down',
+    help = 'Download to Consul'
+  )
+
+  args = parser.parse_args()
+  if args.cmd == 'up':
+    print (f'dir: {args.dir}')
+
+  if args.cmd == 'down':
+    print (f'dir: {args.dir}')
+
+  print (args.cmd)
